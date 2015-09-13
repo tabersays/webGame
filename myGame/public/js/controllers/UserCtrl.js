78fbbc9d1a5845bfc76cd7d477f4f56a8036a765
Taber Storm Fitzgerald <tfitzgerald3@mail.csuchico.edu>
2015-09-13 12:57:49 -0700

    Adds a style page to remove an error

--- myGame/public/js/controllers/UserCtrl.js
+++ myGame/public/js/controllers/UserCtrl.js
@@ -1,13 +1,11 @@
 ﻿angular.module('UserCtrl', []).controller('UserController', ['$scope', 'User', function ($scope, User) {
         $scope.tagline = "Hehe nerds";
         $scope.getUsers = function () {
-            console.log('getting users');
             User.get().then(function (response) {
                 $scope.users = response.data;
             });
         }
         $scope.addUser = function (user) {
-            console.log('Add called');
             User.create(user).then(function () { 
                 User.get().then(function (response) {
                     $scope.users = response.data;
@@ -17,7 +15,6 @@
         }
         
         $scope.deleteUser = function (userId){
-            console.log('deleting user');
             User.delete(userId).then(function () {
                 User.get().then(function (response) {
                     $scope.users = response.data;
